25a68b573f0fedd10debd01b38c93f7e25afdf78
Patrick Tsang <kvtsang@gmail.com>
2018-08-27 12:43:30 +0200

    RCE compression.

--- dune-raw-data/Overlays/RceFragment.hh
+++ dune-raw-data/Overlays/RceFragment.hh
@@ -19,22 +19,34 @@ class TpcStreamUnpack;
 class dune::RceFragment
 {
     public:
+       static const uint64_t PATTERN_WORD = 0x8b309e;
 
-        RceFragment(artdaq::Fragment const & fragment );
-        RceFragment(const uint64_t* data_ptr);
-        int size() const { return _n_streams; }
-        TpcStreamUnpack const * get_stream(int i) const;
-        void hexdump(std::ostream& out, int n_words=10) const;
-        void save(const std::string& filepath) const;
+       RceFragment(artdaq::Fragment const & fragment );
+       RceFragment(const uint64_t* data_ptr);
+       int size() const { return _n_streams; }
+       TpcStreamUnpack const * get_stream(size_t i) const;
+       void hexdump(std::ostream& out, int n_words=10) const;
+       void save(const std::string& filepath) const;
+       const uint64_t* data_ptr() const { return _data_ptr; }
 
-        static RceFragments from_container_frags(const artdaq::Fragments& frags);
+       // quick access to good streams
+       // user can still access individual stream (good or bad)
+       // via get_stream(size_t i)
+       const std::vector<TpcStreamUnpack const *> & get_streams() const {
+          return _streams;
+       }
+
+       static RceFragments from_container_frags(const artdaq::Fragments& frags);
 
     private:
-        void _init();
+       void _init();
+       static const uint64_t * _align(const uint8_t *ptr);
 
-        std::unique_ptr<DataFragmentUnpack> _data_fragment;
-        std::unique_ptr<TpcFragmentUnpack> _tpc_fragment;
-        int _n_streams = 0;
-        const uint64_t* _data_ptr;
+       std::unique_ptr<DataFragmentUnpack> _data_fragment;
+       std::unique_ptr<TpcFragmentUnpack> _tpc_fragment;
+       const uint64_t* _data_ptr;
+
+       size_t _n_streams = 0;
+       std::vector<TpcStreamUnpack const *> _streams;
 };
 #endif 
