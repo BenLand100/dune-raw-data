631261fa545a41f32b390cb746c59349a8f696c6
John Christian Freeman <jofreema@lxplus047.cern.ch>
2018-09-05 20:00:39 +0200

    JCF: make the necessary changes to replicate 324caa724e4247d1e3f0dc167c46da1c3cafea40, i.e., the snapshot of /nfs/sw/work_dirs/dune-artdaq_artdaq_v3_02_01_testing/srcs/dune_raw_data

--- dune-raw-data/Overlays/RceFragment.hh
+++ dune-raw-data/Overlays/RceFragment.hh
@@ -19,34 +19,22 @@ class TpcStreamUnpack;
 class dune::RceFragment
 {
     public:
-       static const uint64_t PATTERN_WORD = 0x8b309e;
 
-       RceFragment(artdaq::Fragment const & fragment );
-       RceFragment(const uint64_t* data_ptr);
-       int size() const { return _n_streams; }
-       TpcStreamUnpack const * get_stream(size_t i) const;
-       void hexdump(std::ostream& out, int n_words=10) const;
-       void save(const std::string& filepath) const;
-       const uint64_t* data_ptr() const { return _data_ptr; }
+        RceFragment(artdaq::Fragment const & fragment );
+        RceFragment(const uint64_t* data_ptr);
+        int size() const { return _n_streams; }
+        TpcStreamUnpack const * get_stream(int i) const;
+        void hexdump(std::ostream& out, int n_words=10) const;
+        void save(const std::string& filepath) const;
 
-       // quick access to good streams
-       // user can still access individual stream (good or bad)
-       // via get_stream(size_t i)
-       const std::vector<TpcStreamUnpack const *> & get_streams() const {
-          return _streams;
-       }
-
-       static RceFragments from_container_frags(const artdaq::Fragments& frags);
+        static RceFragments from_container_frags(const artdaq::Fragments& frags);
 
     private:
-       void _init();
-       static const uint64_t * _align(const uint8_t *ptr);
+        void _init();
 
-       std::unique_ptr<DataFragmentUnpack> _data_fragment;
-       std::unique_ptr<TpcFragmentUnpack> _tpc_fragment;
-       const uint64_t* _data_ptr;
-
-       size_t _n_streams = 0;
-       std::vector<TpcStreamUnpack const *> _streams;
+        std::unique_ptr<DataFragmentUnpack> _data_fragment;
+        std::unique_ptr<TpcFragmentUnpack> _tpc_fragment;
+        int _n_streams = 0;
+        const uint64_t* _data_ptr;
 };
 #endif 
