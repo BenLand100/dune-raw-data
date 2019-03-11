0bf29410dfc27c69ce95cb84ed9b20e83a214f19
Philip Rodrigues <philip.rodrigues@physics.ox.ac.uk>
2019-03-11 17:36:16 +0100

    Store message timestamp and end time in dune::TriggerPrimitive, so that it's more like what comes out of the actual code

--- dune-raw-data/Overlays/FelixHitFormat.hh
+++ dune-raw-data/Overlays/FelixHitFormat.hh
@@ -5,20 +5,23 @@ namespace dune
 {
     struct TriggerPrimitive
     {
-        TriggerPrimitive(uint16_t channel_,
-                         uint64_t startTime_,
+        TriggerPrimitive(uint64_t messageTimestamp_,
+                         uint16_t channel_,
+                         uint16_t endTime_,
                          uint16_t charge_,
                          uint16_t timeOverThreshold_)
-            : channel(channel_), 
-              startTime(startTime_), 
+            : messageTimestamp(messageTimestamp_),
+              channel(channel_), 
+              endTime(endTime_), 
               charge(charge_), 
               timeOverThreshold(timeOverThreshold_)
         {}
 
-        uint16_t channel;
-        uint64_t startTime;
-        uint16_t charge;
-        uint16_t timeOverThreshold;
+        uint64_t messageTimestamp;   // The timestamp of the netio message that this hit comes from
+        uint16_t channel;            // The electronics channel number within the (crate, slot, fiber)
+        uint16_t endTime;            // In TPC ticks relative to the start of the netio message
+        uint16_t charge;             // In ADC
+        uint16_t timeOverThreshold;  // In *TPC* clock ticks
     };
 }
 
