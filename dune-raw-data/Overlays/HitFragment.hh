0cc29dd39a1db0643bcb895f16df298bf9bdbf2c
plasorak <plasorak@fnal.gov>
2019-03-12 19:27:21 +0100

    Add versionning and metadata in the hitfragment.hh

--- dune-raw-data/Overlays/HitFragment.hh
+++ dune-raw-data/Overlays/HitFragment.hh
@@ -13,10 +13,24 @@
 //==================================================
 // FELIX fragment for an array of FELIX hits
 //==================================================
-namespace dune{
+namespace dune {
   
   class HitFragment {
+
   public:
+
+    struct Metadata {
+      typedef uint32_t data_t;
+
+      Metadata(data_t v) : fragment_version(v) {}
+      // "8 bits ought to be enough for anyone"
+      data_t fragment_version : 8;
+      // Give a name to the rest of the bits in case we ever want them for something else
+      data_t unused : 24;
+
+      static size_t const size_words = 1ul; // Units of Metadata::data_t
+    };
+    
     // The following structure is overlayed onto the data in the
     // fragment, starting at the beginning.
     struct Body {
@@ -27,6 +41,8 @@ namespace dune{
     // to refer to the artdaq::Fragment object
     HitFragment(artdaq::Fragment& fragment) : artdaq_Fragment_(fragment) {}
 
+    // Update this version number if you update anything in the class!
+    static constexpr uint32_t VERSION = 0;
 
     const dune::TriggerPrimitive& get_primitive() const { return body_()->primitive; }
     dune::TriggerPrimitive& get_primitive() { return body_()->primitive; }
